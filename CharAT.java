426a1747502c7de93712da96364637f76ca18957
PrasanthV5 <93316256+PrasanthV5@users.noreply.github.com>
2023-06-08 05:49:11 +0530

    CharAt

---
+++ CharAT.java
@@ -0,0 +1,21 @@
+package org.string;
+
+public class CharAT {
+	
+	public static void main(String[] args) {
+		
+		String s1 = "InmakesInfotech";
+		
+		String s2 = "SeleniumAutomationtool";
+		
+		String s3 = "SeleniumAutomationtool";
+		
+		System.out.println(s1.indexOf('h'));
+
+		System.out.println(s2.indexOf('m'));
+		
+		System.out.println(s3.indexOf('u'));
+
+	}
+
+}
